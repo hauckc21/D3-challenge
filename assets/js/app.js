ca93e603fa3d2328d7a02003963682c7b67fa677
hauckc21 <hauck.chris@gmail.com>
2020-10-14 14:30:40 -0500

    svg margins

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 // @TODO: YOUR CODE HERE!
+console.log("This is app.js");
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
